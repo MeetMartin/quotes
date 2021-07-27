4afed211a0dfb4c85c33c6d8b480bfeb3215bbf1
Martin Novak <ragnarecek@gmail.com>
2021-07-27 20:26:18 +1200

    docs: postToFunction documentation

--- src/store/effects/NetlifyFunction.js
+++ src/store/effects/NetlifyFunction.js
@@ -3,6 +3,28 @@ import axios from 'axios';
 
 const api_url = isEqual('development')(process.env.NODE_ENV) ? 'http://localhost:5000' : '/.netlify/functions';
 
+/**
+ * Ansynchronous post request to a Netlify Function on an input path
+ * that maybe returns a returns response data based on input data.
+ * 
+ * @HindleyMilner postToFunction :: string -> object -> AsyncEffect.of(Maybe.of(object))
+ *
+ * @pure
+ * @param {string} path path to the Netlify Function added to /.netlify/functions
+ * @param {object} data data to be posted to the function
+ * @returns {AsyncEffect} AsyncEffect of Maybe of quote object
+ * 
+ * @example
+ * postToFunction('/my-function')({something: 'something'})
+ * .trigger
+ * (error => console.log(error))
+ * (MaybeData =>
+ *  maybe
+ *  (() => console.log('Response data is Nothing.'))
+ *  (data => console.log('Response data are: ', data))
+ *  (MaybeData)
+ * );
+ */
 export const postToFunction = path => data =>
     map(response => Maybe.of(response.data))
     (
